3b9e0b6157986999fe351cc54ad2fbb85735f684
Sébastien Aglaé <38996444+sebastienaglae@users.noreply.github.com>
2024-05-31 21:17:37 +0200

    Add matchmaking buttons

--- game/src/ui/pages/Matchmaking.tsx
+++ game/src/ui/pages/Matchmaking.tsx
@@ -1,10 +1,19 @@
 ﻿import "./matchmaking.css";
 import HanaGamesLogo from "../assets/common/HanaGames.png";
-import {useContext} from "react";
+import React, {useContext} from "react";
 import {PageContext} from "../../index";
+import {PageType} from "../../PageType";
 
 function Matchmaking() {
     const page = useContext(PageContext);
+    const start = () => {
+        console.log("Start clicked");
+    }
+
+    const cancel = () => {
+        console.log("Cancel clicked");
+        page.setPage(PageType.MainMenu);
+    }
     return (
         <>
             <div className='m-background'>
@@ -12,6 +21,13 @@ function Matchmaking() {
                     <h1>Matchmaking</h1>
                     <p>Waiting for players...</p>
                     <p>{page.data.matchmaking.currentPlayers}/{page.data.matchmaking.maxPlayers}</p>
+
+                    <div className='submit'>
+                        <button onClick={start}>Start now</button>
+                        <button className='m-back' onClick={cancel}>
+                            Cancel
+                        </button>
+                    </div>
                 </div>
                 <img className="hanalogo" src={HanaGamesLogo} alt="Hana Games"/>
             </div>
